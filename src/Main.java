dc449d2b62203ca9717c041bc669e82efd57d0d4
kev007 <kev_shrestha@hotmail.com>
2016-01-29 16:22:28 +0100

    crawler2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws IOException, ParseException,
+    ClassNotFoundException, SQLException, InvalidKeyException,
+    NoSuchAlgorithmException, InterruptedException {
         Parent root = FXMLLoader.load(getClass().getResource("MainWindowView.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -15,7 +24,15 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) throws IOException, ParseException,
+            ClassNotFoundException, SQLException, InvalidKeyException,
+            NoSuchAlgorithmException, InterruptedException {
+
+        //launch(args);
+
+        //example: write all reviews for an item (defined by its ASIN) to a SQLite database
+        Item an_item = new Item("B00EB7UIZU");
+        an_item.fetchReview();
+        an_item.writeReviewsToDatabase("test.db", false);
     }
 }
--- src/crawler/DEPRECATEDcrawler.java
+++ src/crawler/DEPRECATEDcrawler.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.io.IOException;
 
-public class crawler {
+public class DEPRECATEDcrawler {
 
 	/**
 	 * @param args
